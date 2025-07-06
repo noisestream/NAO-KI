cf588f9099ca9a9ef416ef0f361fc4d55320da72
noisestream <me@noisestream.de>
2025-07-06 17:56:22 +0200

    Webserver-Setup

--- server/webserver.py
+++ server/webserver.py
@@ -94,3 +94,21 @@ class Root(object):
         return {'status': 'ok', 'command': command}
 
 # Webserver-Setup
+
+def start_webserver(host="0.0.0.0", port=9000):
+    cherrypy.config.update({
+        "server.socket_host": host,
+        "server.socket_port": port
+    })
+    WebSocketPlugin(cherrypy.engine).subscribe()
+    cherrypy.tools.websocket = WebSocketTool()
+    config = {
+        "/ws": {"tools.websocket.on": True,
+                 "tools.websocket.handler_cls": CommandWebSocket}
+    }
+    cherrypy.tree.mount(Root(), "/", config)
+    cherrypy.engine.start()
+
+
+def stop_webserver():
+    cherrypy.engine.exit()
